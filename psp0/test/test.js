fe8a70bb24cdcb62ca82fd77db7b737f71b9af8d
milena <mile.card195@gmail.com>
2017-03-09 15:32:42 -0500

    Prueba coverage

--- psp0/test/test.js
+++ psp0/test/test.js
@@ -3,28 +3,19 @@ var expect = require('chai').expect;
 var mf= null;
 
 
-describe('Testing the tests',function(){
+describe('Pruebas operaciones matematicas',function(){
 
-  it('Should pass when everything is ok',function(){
-    expect(true).to.be.true;
-  });
-
-  it('Should success',function(){
-    expect(true).to.be.true;
-  });
-
-});
-
-
-describe('Suma',function(){
   //Carga los metodos del objeto antes de cada 'it'
   beforeEach(function(){
     mf= new MathFunctions();
 
   });
+
   it('Deberia sumar dos numeros',function(){
-
     expect(mf.suma(10,12)).to.equal(22);
+  });
 
+  it('Deberia calcular la media de un grupo de numeros', function(){
+    expect(mf.average(numberArray)).to.equal(20);
   });
 });
